2114cfd5ad219d74095b6d3f2626a3e5e2bb2801
KJ Osenenko <karljosenenko@gmail.com>
2021-04-05 17:38:56 -0600

    rendered lineup in component

--- src/components/Lineup.js
+++ src/components/Lineup.js
@@ -1,13 +1,21 @@
 import React, { Component } from 'react'
+import {connect} from 'react-redux'
 
 class Lineup extends Component {
     render() {
+        const lineup = this.props.lineup.map((band, i) => <p>{band.band_id}</p>)
         return (
             <div>
-                Lineup
+                {lineup}
             </div>
         )
     }
 }
 
-export default Lineup
+const mapStateToProps = state => {
+    return {
+        lineup: state.lineup
+    }
+}
+
+export default connect(mapStateToProps)(Lineup)
