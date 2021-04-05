a19b8955acdd19ba07ee2aa49add068c6401dd97
KJ Osenenko <karljosenenko@gmail.com>
2021-04-05 14:41:57 -0600

    added basic routing

--- src/index.js
+++ src/index.js
@@ -1,14 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import './index.css';
 import {Provider} from 'react-redux'
-import {createStore, applymiddleware} from 'redux'
+import {createStore, applyMiddleware} from 'redux'
+import thunk from 'redux-thunk'
+import scheduleReducer from './reducers/scheduleReducer'
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+const store = createStore(scheduleReducer, applyMiddleware(thunk))
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
---
+++ src/reducers/scheduleReducer.js
@@ -0,0 +1,13 @@
+const initialState = {
+    slots: [],
+    loading: true
+}
+
+const scheduleReducer = (state, action) => {
+    switch(action.type) {
+        default:
+            return state
+    }
+}
+
+export default scheduleReducer
