33faf5ff0ab0ec5b0c7e1aed754d124df015091a
KJ Osenenko <karljosenenko@gmail.com>
2021-04-07 17:56:32 -0600

    rendered venues component

--- src/components/Venues.js
+++ src/components/Venues.js
@@ -1,13 +1,21 @@
 import React, { Component } from 'react'
-
+import {connect} from 'react-redux'
+ 
 class Venues extends Component {
     render() {
+        const venues = this.props.venues.map((venue, i) => <p>{venue.name}</p>)
         return (
             <div>
-                Venues
+                {venues}
             </div>
         )
     }
 }
 
-export default Venues
+const mapStateToProps = state => {
+    return {
+        venues: state.venues.venues
+    }
+}
+
+export default connect(mapStateToProps)(Venues)
