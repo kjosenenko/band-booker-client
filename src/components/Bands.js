b074ee534e5db73cad6fd20594f38f6ab71ccf9b
KJ Osenenko <karljosenenko@gmail.com>
2021-04-07 18:08:24 -0600

    rendered artists component

--- src/components/Bands.js
+++ src/components/Bands.js
@@ -1,13 +1,21 @@
 import React, { Component } from 'react'
+import {connect} from 'react-redux'
 
 class Bands extends Component {
     render() {
+        const bands = this.props.bands.map((band, i) => <p>{band.name}</p>)
         return (
             <div>
-                Bands
+                {bands}
             </div>
         )
     }
 }
 
-export default Bands
+const mapStateToProps = state => {
+    return {
+        bands: state.bands.bands
+    }
+}
+
+export default connect(mapStateToProps)(Bands)
